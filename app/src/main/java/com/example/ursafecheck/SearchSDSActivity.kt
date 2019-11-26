267459fd0c0b093a9f2b38c1f7d1f095d93f7d8c
Harsh Patel <hrshpatelz472@gmail.com>
2019-11-26 02:38:22 -0600

    Search SDS  screen is almost ready and Lab Safety Manual screen is completed

--- app/src/main/java/com/example/ursafecheck/SearchSDSActivity.kt
+++ app/src/main/java/com/example/ursafecheck/SearchSDSActivity.kt
@@ -2,11 +2,54 @@ package com.example.ursafecheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mancj.materialsearchbar.MaterialSearchBar
+import android.widget.Toast
 
-class SearchSDSActivity : AppCompatActivity() {
+
+class SearchSDSActivity : AppCompatActivity(), MaterialSearchBar.OnSearchActionListener {
+
+    lateinit var searchBar: MaterialSearchBar
+    private lateinit var lastSearches:List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_sds)
+
+        searchBar  = findViewById(R.id.searchBar) as MaterialSearchBar
+        searchBar.setHint("Methanol")
+        searchBar.setSpeechMode(true)
+        searchBar.setOnSearchActionListener(this)
+    //    lastSearches = loadSearchSuggestionFromDisk()
+       // searchBar.setLastSuggestions(list)
+     //   searchBar.inflateMenu(R.menu.Main)
+     //   searchBar.getMenu().setOnMenuItemClickListener(this)
+
     }
+
+   /* override fun onDestroy() {
+        super.onDestroy()
+    //    saveSearchSuggestionToDisk(searchBar.getLastSuggestions());
+    }*/
+    override fun onSearchStateChanged(enabled: Boolean) {
+
+        val state = if (true) "enabled" else "disabled"
+        Toast.makeText(this, "Search $state", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSearchConfirmed(text: CharSequence?) {
+        val modifiedText:String = text.toString()
+        val commonTextAppender = "SDS"
+        startSearch("$modifiedText $commonTextAppender", true, null, true);
+
+    }
+
+    override fun onButtonClicked(buttonCode: Int) {
+        when (buttonCode) {
+   //         MaterialSearchBar.BUTTON_NAVIGATION -> drawer.openDrawer(Gravity.LEFT)
+     //       MaterialSearchBar.BUTTON_SPEECH -> openVoiceRecognizer()
+        }
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
--- app/src/main/java/com/example/ursafecheck/UserMannualActivity.kt
+++ app/src/main/java/com/example/ursafecheck/UserMannualActivity.kt
@@ -2,11 +2,63 @@ package com.example.ursafecheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.shockwave.pdfium.PdfDocument;
 
-class UserMannualActivity : AppCompatActivity() {
+import android.util.Log
+
+
+class UserMannualActivity : AppCompatActivity(), OnPageChangeListener, OnLoadCompleteListener {
+
+    private val TAG = MainActivity::class.java.simpleName
+    val SAMPLE_FILE = "Faculty of Science Laboratory Safety Manual.pdf"
+    var pdfView: PDFView? = null
+    var pageNumber: Int? = 0
+    var pdfFileName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_mannual)
+
+        pdfView = findViewById(R.id.pdfView) as PDFView
+        displayFromAsset(SAMPLE_FILE)
     }
+
+    private fun displayFromAsset(assetFileName: String) {
+        pdfFileName = assetFileName
+
+        pageNumber?.let {
+            pdfView?.fromAsset(SAMPLE_FILE)?.defaultPage(it)?.enableSwipe(true)
+                ?.swipeHorizontal(false)
+                ?.onPageChange(this)
+                ?.enableAnnotationRendering(true)
+                ?.onLoad(this)
+                ?.scrollHandle(DefaultScrollHandle(this))
+                ?.load()
+        }
+    }
+    override fun onPageChanged(page: Int, pageCount: Int) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+
+    override fun loadComplete(nbPages: Int) {
+        val meta = pdfView?.getDocumentMeta()
+        printBookmarksTree(pdfView?.getTableOfContents() as MutableList<PdfDocument.Bookmark>, "-")
+    }
+
+    fun printBookmarksTree(tree: MutableList<PdfDocument.Bookmark>, sep: String) {
+        for (b in tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.title, b.pageIdx))
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.children, "$sep-")
+            }
+        }
+    }
+
 }
