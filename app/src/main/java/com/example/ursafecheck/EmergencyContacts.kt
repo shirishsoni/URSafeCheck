32619224c4ea5ce63b49fd3d20b3a5e3bee589aa
Shirish Soni <shirishsoni12@gmail.com>
2019-12-12 01:12:57 -0600

    Emergency contacts updated

--- app/src/main/java/com/example/ursafecheck/EmergencyContacts.kt
+++ app/src/main/java/com/example/ursafecheck/EmergencyContacts.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.activity_emergency_contacts.*
+import android.net.Uri
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class EmergencyContacts : AppCompatActivity() {
 
@@ -17,22 +23,24 @@ class EmergencyContacts : AppCompatActivity() {
     fun CardActivity(view: View) {
         if((view as CardView) == em_c1)
         {
-            // launching homepage
-            val intent = Intent(this, HomapageActivity::class.java)
+            // launching phone app with 911 number
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:911")
             startActivity(intent)
 
         }
         else if((view as CardView) == em_c2)
         {
-            // launching homepage
-            val intent = Intent(this, HomapageActivity::class.java)
+            // launching phone app with advisors number
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:306-585-4769")
             startActivity(intent)
-
         }
         else
         {
-            // launching homepage
-            val intent = Intent(this, HomapageActivity::class.java)
+            // launching phone app with campus security number
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:306-585-4999")
             startActivity(intent)
         }
     }
