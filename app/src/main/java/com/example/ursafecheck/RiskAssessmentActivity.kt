b2cec18f22e8f4f3092b67aa8372060c8b7107b1
Shirish <shirishsoni12@gmail.com>
2020-10-28 17:58:36 -0400

    Risk Assessment form UI completed

--- app/src/main/java/com/example/ursafecheck/RiskAssessmentActivity.kt
+++ app/src/main/java/com/example/ursafecheck/RiskAssessmentActivity.kt
@@ -1,13 +1,72 @@
 package com.example.ursafecheck
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import java.util.*
 
 //For future development
-class RiskAssessmentActivity : AppCompatActivity() {
+class RiskAssessmentActivity : AppCompatActivity(), View.OnClickListener {
+
+    // Private variables for the Date and time Picker
+    private var mYear: Int = 0
+    private var mMonth: Int = 0
+    private var mDay: Int = 0
+    private var mHour: Int = 0
+    private var mMinute: Int = 0
+
+    // lazy initialization of Date and Time Picker
+    lateinit var btnDatePicker: MaterialButton
+    lateinit var btnTimePicker: MaterialButton
+    lateinit var txtDate: EditText
+    lateinit var txtTime: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_risk_assessment)
+    }
+
+    override fun onClick(view: View) {
+
+        //code for time picker handler
+        if(view === btnTimePicker){
+
+            val c = Calendar.getInstance()
+            mHour = c.get(Calendar.HOUR_OF_DAY)
+            mMinute = c.get(Calendar.MINUTE)
+
+            // Dialog for Time Picker
+            val timePickerDialog = TimePickerDialog(this,
+                //
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    txtTime?.setText("$hourOfDay:$minute")
+                }, mHour, mMinute, false
+            )
+            // enabling the view of time Picker
+            timePickerDialog.show()
+        }
+
+        // code for time picker handler
+        if(view === btnDatePicker) {
+
+            val c = Calendar.getInstance()
+            mYear = c.get(Calendar.YEAR)
+            mMonth = c.get(Calendar.MONTH)
+            mDay = c.get(Calendar.DAY_OF_MONTH)
+
+            // Dialog for the Data Picker
+            val datePickerDialog = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    txtDate?.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                }, mYear, mMonth, mDay
+            )
+            //enabling the view of date Picker
+            datePickerDialog.show()
+        }
+
     }
 }
