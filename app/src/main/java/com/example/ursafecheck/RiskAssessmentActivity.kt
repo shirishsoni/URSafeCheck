1ffe8dc51c1b4120c56dac791aee3b16b68dc853
Shirish <shirishsoni12@gmail.com>
2021-01-04 18:16:39 -0500

    addition to risk assessment form.

--- app/src/main/java/com/example/ursafecheck/RiskAssessmentActivity.kt
+++ app/src/main/java/com/example/ursafecheck/RiskAssessmentActivity.kt
@@ -2,11 +2,13 @@ package com.example.ursafecheck
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_risk_assessment.*
 import java.util.*
 
 //For future development
@@ -15,5 +17,10 @@ class RiskAssessmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_risk_assessment)
+
+        ra_cnt_btn.setOnClickListener(){
+            val intent = Intent(this, HazardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
