1fa1b8ddfa27c48d2132262675a4f2b104963589
Shirish <shirishsoni12@gmail.com>
2021-01-04 21:17:48 -0500

    API development

--- app/src/main/java/com/example/ursafecheck/RESTful_API_and_sql_db/main.py
+++ app/src/main/java/com/example/ursafecheck/RESTful_API_and_sql_db/main.py
@@ -13,6 +13,8 @@ from random import randint
 #For email
 import smtplib, ssl
 
+ra_msg = "Risk Assessment Form details: "
+
 app = Flask(__name__)
 
 # Different functions have been created for different requests
@@ -285,11 +287,135 @@ def irForm():
     cursor.close()
     conn.close()
 
-#Code for submitting Incident Reporting form
-@app.route('/api/irForm', methods=['POST'])
+#Code for fetching Risk Assessment form1
+@app.route('/api/raForm1', methods=['POST'])
 def raForm1():
-    print("Risk assessment for submitted") 
+    
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    json = request.get_json()
+    Ename = json['Ename']
+    sup_name = json['sup_name']
+    job_title = json['job_title']
+    dept = json['dept']
+    mob_no = json['mob_no']
+    email = json['email']
+    sup_email = json['sup_email']
+    sup_no = json['sup_no']
+    emo_no = json['emo_no']
+    adv_name = json['adv_name']
+    location = json['location']
+    date = json['date']
+    time = json['time']
+    task = json['task']
+    rb1_ques = json['rb1_ques']
+    rb1_value = json['rb1_value']
+    rb2_ques = json['rb2_ques']
+    rb2_value = json['rb2_value']
+    rb3_ques = json['rb3_ques']
+    rb3_value = json['rb3_value']
+    rb4_ques = json['rb4_ques']
+    rb4_value = json['rb4_value']
+    rb5_ques = json['rb5_ques']
+    rb5_value = json['rb5_value']
+    rb6_ques = json['rb6_ques']
+    rb6_value = json['rb6_value']
+    rb7_ques = json['rb7_ques']
+    rb7_value = json['rb7_value']
+    rb8_ques = json['rb8_ques']
+    rb8_value = json['rb8_value']
+    rb9_ques = json['rb9_ques']
+    rb9_value = json['rb9_value']
+    rb10_ques = json['rb10_ques']
+    rb10_value = json['rb10_value']
+    rb11_ques = json['rb11_ques']
+    rb11_value = json['rb11_value']
+    rb12_ques = json['rb12_ques']
+    rb12_value = json['rb12_value']
+    rb13_ques = json['rb13_ques']
+    rb13_value = json['rb13_value']
+    rb14_ques = json['rb14_ques']
+    rb14_value = json['rb14_value']
+    rb15_ques = json['rb15_ques']
+    rb15_value = json['rb15_value']
+    rb16_ques = json['rb16_ques']
+    rb16_value = json['rb16_value']
+    rb17_ques = json['rb17_ques']
+    rb17_value = json['rb17_value']
+    rb18_ques = json['rb18_ques']
+    rb18_value = json['rb18_value']
+    rb19_ques = json['rb19_ques']
+    rb19_value = json['rb19_value']
+    rb20_ques = json['rb120_ques']
+    rb20_value = json['rb20_value']
 
+    
+    ra_msg = "Subject: Risk Assessment Report Submitted \n\nEmployee Name: "+str(Ename)+\
+              "\n\nSupervisor Name: "+str(sup_name)+"\n\nJob Title: "+str(job_title)+"\n\nDepartment: "+str(dept)+\
+              "\n\nUser mobile number: "+str(mob_no)+"\n\nUser Email: "+str(email)+\
+              "\n\nSupervisor Email: "+str(sup_email)+"\n\Emergency number: "+str(emo_no)+\
+              "\n\nAdvisor Name: "+str(adv_name)+\
+              "\n\Location: "+str(location)+"\n\nDate: "+str(date)+\
+              "\n\nTime: "+str(time)+"\n\nTask: "+str(task)+\
+              "\n\n" + str(rb1_ques)+"\n\n: "+str(rb1_value)+\
+              "\n\n" + str(rb2_ques)+"\n\n: "+str(rb2_value)+\
+              "\n\n" + str(rb3_ques)+"\n\n: "+str(rb3_value)+\
+              "\n\n" + str(rb4_ques)+"\n\n: "+str(rb4_value)+\
+              "\n\n" + str(rb5_ques)+"\n\n: "+str(rb5_value)+\
+              "\n\n" + str(rb6_ques)+"\n\n: "+str(rb6_value)+\
+              "\n\n" + str(rb7_ques)+"\n\n: "+str(rb7_value)+\
+              "\n\n" + str(rb8_ques)+"\n\n: "+str(rb8_value)+\
+              "\n\n" + str(rb9_ques)+"\n\n: "+str(rb9_value)+\
+              "\n\n" + str(rb10_ques)+"\n\n: "+str(rb10_value)+\
+              "\n\n" + str(rb11_ques)+"\n\n: "+str(rb11_value)+\
+              "\n\n" + str(rb12_ques)+"\n\n: "+str(rb12_value)+\
+              "\n\n" + str(rb13_ques)+"\n\n: "+str(rb13_value)+\
+              "\n\n" + str(rb14_ques)+"\n\n: "+str(rb14_value)+\
+              "\n\n" + str(rb15_ques)+"\n\n: "+str(rb15_value)+\
+              "\n\n" + str(rb16_ques)+"\n\n: "+str(rb16_value)+\
+              "\n\n" + str(rb17_ques)+"\n\n: "+str(rb17_value)+\
+              "\n\n" + str(rb18_ques)+"\n\n: "+str(rb18_value)+\
+              "\n\n" + str(rb19_ques)+"\n\n: "+str(rb19_value)+\
+              "\n\n" + str(rb20_ques)+"\n\n: "+str(rb20_value)
+              
+
+    resp = jsonify({'response':'Page1 accepted'})
+    resp.status_code = 200
+    return resp
+    cursor.close()
+    conn.close()
+
+#Code for submitting Hazards form
+@app.route('/api/raForm2', methods=['POST'])
+def raForm2():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    json = request.get_json()
+    hazards = json['hazards']
+    hazards = hazards.replace('[', '')
+    hazards = hazards.replace(']', '')
+    hazards = hazards.replace('Hazard:', '\nHazard:')
+
+    ra_msg = ra_msg + str(hazards)
+
+    #Sending the email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, "shirishsoni12@gmail.com", ra_msg)
+        
+    resp = jsonify({'response':'Risk Assessment Form submitted'})
+    resp.status_code = 200
+    return resp
+    cursor.close()
+    conn.close()
+    
+    
 # running web app in local machine
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+
+    
